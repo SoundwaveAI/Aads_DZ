945178ff665b26146f74da726ee89d7097e44801
SoundwaveAI <ghost10.town10000@gmail.com>
2026-03-04 17:48:43 +0300

    dobavila dlia proverki raboti

---
+++ testfun.hpp
@@ -0,0 +1,20 @@
+#ifndef TESTFUN_HPP
+#define TESTFUN_HPP
+#include <iostream>
+
+template < class T >
+struct SumM {
+  size_t total = 0;
+  void operator()(const T& val) {
+    total += val;
+  }
+};
+
+template < class T >
+struct PrintM {
+  void operator()(const T& val) {
+    std::cout << val << " ";
+  }
+};
+
+#endif
